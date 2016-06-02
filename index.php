16489115d11db4b287fcc123907ad0a5d6b682d0
Pasan Ranathunga <Pasan Ranathunga>
2016-06-02 08:05:57 +0530

    map n results added

--- index.php
+++ index.php
@@ -9,8 +9,79 @@
 
 	<script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 </head>
 <body>
+<script type="text/javascript">
+    var geocoder = new google.maps.Geocoder();
+
+    function geocodePosition(pos) {
+        geocoder.geocode({
+            latLng: pos
+        }, function(responses) {
+            if (responses && responses.length > 0) {
+                updateMarkerAddress(responses[0].formatted_address);
+            } else {
+                updateMarkerAddress('Cannot determine address at this location.');
+            }
+        });
+    }
+
+    function updateMarkerStatus(str) {
+        //document.getElementById('markerStatus').innerHTML = str;
+    }
+
+    function updateMarkerPosition(latLng) {
+       /* document.getElementById('latitude').innerHTML = [
+            latLng.lat(),
+            latLng.lng()
+        ].join(', ');*/
+        console.log(latLng.lat());
+        $("#latitude").val(latLng.lat());
+        $("#longitude").val(latLng.lng());
+    }
+
+    function updateMarkerAddress(str) {
+        //document.getElementById('address').innerHTML = str;
+    }
+
+    function initialize() {
+        var latLng = new google.maps.LatLng(-34.397, 150.644);
+        var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+            zoom: 8,
+            center: latLng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        var marker = new google.maps.Marker({
+            position: latLng,
+            title: 'Point A',
+            map: map,
+            draggable: true
+        });
+
+        // Update current position info.
+        updateMarkerPosition(latLng);
+        geocodePosition(latLng);
+
+        // Add dragging event listeners.
+        google.maps.event.addListener(marker, 'dragstart', function() {
+            updateMarkerAddress('Dragging...');
+        });
+
+        google.maps.event.addListener(marker, 'drag', function() {
+            updateMarkerStatus('Dragging...');
+            updateMarkerPosition(marker.getPosition());
+        });
+
+        google.maps.event.addListener(marker, 'dragend', function() {
+            updateMarkerStatus('Drag ended');
+            geocodePosition(marker.getPosition());
+        });
+    }
+
+    // Onload handler to fire off the app.
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 
 <div class="container-fluid ">
     <div class="row">
@@ -26,10 +97,13 @@
                 <div class="login-container">
                     <form class="form-inline">
                         <div class="form-group">
-                            <input placeholder="Longitude" type="text" name="longitude" id="longitude" class="form-control">
+                            <label>Latitude:</label>
+                            <input placeholder="Latitude"  type="text" name="latitude" id="latitude" class="form-control">
                         </div>
                         <div class="form-group">
-                            <input placeholder="Latitude"  type="text" name="latitude" id="latitude" class="form-control">
+                            <label>Longitude:</label>
+                            <input placeholder="Longitude" type="text" name="longitude" id="longitude" class="form-control">
+
                         </div>
                         <input type="button" value="Submit" id="submitButton" class="btn btn-md btn-success">
                     </form>
@@ -39,11 +113,13 @@
 
             </div>
             <div class="weather-display col-lg-12">
-                <div id="map" class="col-lg-7"></div>
+                <div id="map" class="col-lg-7">
+                    <div id="mapCanvas"></div>
+                </div>
                 <div id="results" class="col-lg-5">
                     <div class="row results-name">
                         <div class="results-icon col-lg-5">
-                            <div class="icon thunder-storm" id="thunder-storm">
+                            <div class="icon thunder-storm hidden" id="thunder-storm">
                                 <div class="cloud"></div>
                                 <div class="lightning">
                                     <div class="bolt"></div>
@@ -84,21 +160,20 @@
                             </div>
                         </div>
                         <div class="results-city col-lg-7">
-                            <label id="r-city-name">Pasan</label>
+                            <label id="r-city-name"></label>
                         </div>
                     </div>
 
-                    <div class="result-table">
+                    <div class="result-table" id="result-table">
                         <table class="table table-striped table-bordered table-condensed">
                             <tbody>
-
-                            <tr><td>Description</td><td id="description">Gentle Breeze 4.1 m/s</td></tr>
-                            <tr><td>Cloudiness</td><td id="cloudiness">broken clouds</td></tr>
-                            <tr><td>Pressure<br></td><td id="pressure">1020 hpa</td></tr>
-                            <tr><td>Humidity</td><td id="humidity">93 %</td></tr>
-                            <tr><td>Rain</td><td id="rain">0.25</td></tr>
-                            <tr><td>Sunrise</td><td id="sunrise">9:19</td></tr>
-                            <tr><td>Sunset</td><td id="sunset">1:39</td></tr>
+                            <tr><td>Type</td><td id="type"></td></tr>
+                            <tr><td>Description</td><td id="description"></td></tr>
+                            <tr><td>Pressure<br></td><td id="pressure"></td></tr>
+                            <tr><td>Humidity</td><td id="humidity"></td></tr>
+                            <tr><td>Windspeed</td><td id="windspeed"></td></tr>
+                            <tr><td>Sunrise</td><td id="sunrise"></td></tr>
+                            <tr><td>Sunset</td><td id="sunset"></td></tr>
                             </tbody>
                         </table>
                     </div>
@@ -121,6 +196,7 @@
     $('#submitButton').click(function(){
         var longitude = $('#longitude').val();
         var latitude = $('#latitude').val();
+        console.log(longitude);
         ajaxSend(longitude,latitude);
     });
 
@@ -128,22 +204,81 @@
     function ajaxSend(longitude,latitude){
         $.ajax({
             type: "POST",
-            url: "http://pmartz:8280/services/pmartz?lat=" + latitude + "&lon=" + longitude,
-            data : {
-                longitude: longitude,
-                latitude: latitude
-            },
+            url: "http://PMArtz:8280/services/pmartz?lat=" + latitude + "&lon=" + longitude,
             dataType: "json",
             success: function(response){
-                alert(response['city']);
+                var name = response['name'];
+                var type = response['main'];
+                var description = response['description'];
+                var pressure = response['pressure'];
+                var humidity = response['humidity'];
+                var windspeed = response['windspeed'];
+                var sunrise = response['sunrise'];
+                var sunset = response['sunset'];
+                var icon = response['icon'];
+                console.log(icon);
+                $('#r-city-name, #description,#type,#pressure,#humidity,#windspeed,#sunrise,#sunset').fadeOut(500,function(){
+                    $('#r-city-name, #description,#type,#pressure,#humidity,#windspeed,#sunrise,#sunset').empty();
+                    document.getElementById("r-city-name").innerHTML = name;
+                    document.getElementById("description").innerHTML = description;
+                    document.getElementById("type").innerHTML = type;
+                    document.getElementById("pressure").innerHTML = pressure;
+                    document.getElementById("humidity").innerHTML = humidity;
+                    document.getElementById("windspeed").innerHTML = windspeed;
+                    document.getElementById("sunrise").innerHTML = sunrise;
+                    document.getElementById("sunset").innerHTML = sunset;
+                    $('#r-city-name, #description,#type,#pressure,#humidity,#windspeed,#sunrise,#sunset').fadeIn(500);
+                });
+
+                if(icon == '01d' || icon == '01n'){
+                    $('#thunder-storm,#cloudy,#flurries,#rainy,#sun-shower,#sunny').addClass('hidden');
+                    $('#sunny').removeClass('hidden');
+                }
+                else if(icon == '02d' ||  icon == '02n' ||  icon == '03d' ||  icon == '03n' ||  icon == '04d' ||  icon == '04n'){
+                    $('#thunder-storm,#cloudy,#flurries,#rainy,#sun-shower,#sunny').addClass('hidden');
+                    $('#cloudy').removeClass('hidden');
+                }
+                else if(icon == '10d'){
+                    $('#thunder-storm,#cloudy,#flurries,#rainy,#sun-shower,#sunny').addClass('hidden');
+                    $('#sun-shower').removeClass('hidden');
+
+                }
+                else if(icon == '09d' || icon == '09n' || icon == '10n'){
+                    $('#thunder-storm,#cloudy,#flurries,#rainy,#sun-shower,#sunny').addClass('hidden');
+                    $('#rainy').removeClass('hidden');
+                }
+                else if(icon = '11d' || icon == '11n'){
+                    $('#thunder-storm,#cloudy,#flurries,#rainy,#sun-shower,#sunny').addClass('hidden');
+                    $('#thunder-storm').removeClass('hidden');
+
+                }
+                else if(icon == '13d' || icon == '13n'){
+                    $('#thunder-storm,#cloudy,#flurries,#rainy,#sun-shower,#sunny').addClass('hidden');
+                    $('#flurries').removeClass('hidden');
+
+                }
+
+
             },
             error: function(xhr, status, error){
                 console.log(xhr);
                 console.log(status,error);
+            },
+            fail:function(response){
+                alert('failed');
             }
 
         });
 
+    }
+
+    function addData(name,type,description,pressure,humidity,windspeed,sunrise,sunset){
+        console.log(name);
+        //$('#r-city-name, #description,#cloudiness,#pressure,#humidity,#rain,#sunrise,#sunset').empty();
+        $('#r-city-name, #description,#type,#pressure,#humidity,#windspeed,#sunrise,#sunset').fadeOut(500,function(){
+            $('#r-city-name, #description,#cloudiness,#pressure,#humidity,#rain,#sunrise,#sunset').empty();
+
+        });
     }
 
 </script>
--- weatherAppController.php
+++ weatherAppController.php
@@ -1,7 +1,76 @@
-<?php
-
-if(isset($_POST) && count($_POST)){
-    $longitude = $_POST['longitude'];
-    $latitude = $_POST['latitude'];
-    //echo json_encode(array("success" => "0","id" => $a));
-}
+<?xml version="1.0" encoding="UTF-8"?>
+<proxy xmlns="http://ws.apache.org/ns/synapse"
+       name="WeatherAPI"
+       transports="https,http"
+       statistics="disable"
+       trace="disable"
+       startOnLoad="true">
+    <target>
+        <inSequence>
+            <property name="uri.var.lat"
+                      expression="$url:lat"
+                      scope="default"
+                      type="STRING"/>
+            <property name="uri.var.lon"
+                      expression="$url:lon"
+                      scope="default"
+                      type="STRING"/>
+            <call>
+                <endpoint>
+                    <http uri-template="http://nominatim.openstreetmap.org/reverse?lat={uri.var.lat}&amp;lon={uri.var.lon}"/>
+                </endpoint>
+            </call>
+            <property name="uri.var.city"
+                      expression="//reversegeocode/addressparts/city"
+                      scope="default"
+                      type="STRING"/>
+            <property name="uri.var.appid"
+                      value="174e64c6c78872d28494a94c1d450c59"
+                      scope="default"
+                      type="STRING"/>
+            <send>
+                <endpoint>
+                    <http uri-template="http://api.openweathermap.org/data/2.5/weather?q={uri.var.city}&amp;appid={uri.var.appid}"/>
+                </endpoint>
+            </send>
+        </inSequence>
+        <outSequence>
+            <payloadFactory media-type="json">
+                <format>{
+                    "name":"$1",
+                    "main":"$2",
+                    "description":"$3",
+                    "icon":"$4",
+                    "temp":"$5",
+                    "temp_min":"$6",
+                    "temp_max":"$7",
+                    "pressure":"$8",
+                    "humidity":"$9",
+                    "windspeed":"$10",
+                    "country":"$11",
+                    "sunrise":"$12",
+                    "sunset":"$13",
+                    "cod":"$14"
+                    }</format>
+                <args>
+                    <arg expression="$.name" evaluator="json"/>
+                    <arg expression="$.weather[0].main" evaluator="json"/>
+                    <arg expression="$.weather[0].description" evaluator="json"/>
+                    <arg expression="$.weather[0].icon" evaluator="json"/>
+                    <arg expression="$.main.temp" evaluator="json"/>
+                    <arg expression="$.main.temp_min" evaluator="json"/>
+                    <arg expression="$.main.temp_max" evaluator="json"/>
+                    <arg expression="$.main.pressure" evaluator="json"/>
+                    <arg expression="$.main.humidity" evaluator="json"/>
+                    <arg expression="$.wind.speed" evaluator="json"/>
+                    <arg expression="$.sys.country" evaluator="json"/>
+                    <arg expression="$.sys.sunrise" evaluator="json"/>
+                    <arg expression="$.sys.sunset" evaluator="json"/>
+                    <arg expression="$.cod" evaluator="json"/>
+                </args>
+            </payloadFactory>
+            <send/>
+        </outSequence>
+    </target>
+    <description/>
+</proxy>
